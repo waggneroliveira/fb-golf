7955c624c513738cb62ba19ca6e5ef79cadeb29d
Front-end-4 <waggner.dev@gmail.com>
2026-08-04 17:42:20 -0300

    Update client.blade.php

--- resources/views/client/core/client.blade.php
+++ resources/views/client/core/client.blade.php
@@ -159,9 +159,9 @@
                                     A Federação <i class="bi bi-chevron-down"></i>
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
+                                    <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#abou">Sobre</a></li>
                                     <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board"
-       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Sobre</a></li>
-                                    <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board">Diretoria</a></li>
+       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Diretoria</a></li>
                                 </ul>
 
                             </li>
@@ -289,9 +289,9 @@
                             A Federação <i class="bi bi-chevron-down"></i>
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
+                            <li class="mx-2 my-1"><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#abou">Sobre</a></li>
                             <li class="mx-2 my-1"><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board"
-       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Sobre</a></li>
-                            <li class="mx-2 my-1"><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board">Diretoria</a></li>
+       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Diretoria</a></li>
                         </ul>
 
                     </li>
@@ -399,9 +399,9 @@
                             A Federação <i class="bi bi-chevron-down"></i>
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
+                            <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#abou">Sobre</a></li>
                             <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board"
-       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Sobre</a></li>
-                            <li><a class="dropdown-item montserrat-medium text-start font-15" href="{{ route('index') }}#board">Diretoria</a></li>
+       onclick="event.preventDefault(); document.getElementById('board').scrollIntoView({behavior:'smooth'});">Diretoria</a></li>
                         </ul>
 
                     </li>
